689284f1d03724ca2bcad123af35f5dd0d3b7c34
sasonhe <642139009@qq.com>
2019-06-28 18:18:49 +0800

    arr push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="home">
     <div class="title">
-      <h1>首页...</h1>
+      <h1 @click="log()">{{title}}</h1>
     </div>
+    <van-index-bar>
+      <van-index-anchor index="A" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+
+      <van-index-anchor index="B" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+      <van-cell title="文本" />
+
+    </van-index-bar>
+
     <div class="btn">
       <van-button @click="loginOut()"   size="normal" block round  type="primary" text="退出登录"/>
     </div>
@@ -12,10 +25,31 @@
 <script>
 export default {
   name: 'home',
+  data(){
+    return{
+      title:'全国车牌号索引',
+      cjson:require('@/assets/CJSON.json')
+    }
+  },
   components: {
 
   },
   methods:{
+    log(){
+      let pArr = [];
+      this.cjson.map((item,i)=>{
+        pArr.push(item.province)
+      })
+      let afterArr = this.unique(pArr);//所有省份
+      console.log(afterArr);
+    },
+    unique(arr) {
+      if (!Array.isArray(arr)) {
+        console.log('type error!')
+        return
+      }
+      return Array.from(new Set(arr))
+    },
     loginOut(){
       this.$store.dispatch('user/logout').then((res) => {
         console.log(res)
